8a5e1007368cd91a99348edcd494926381de4079
zivklein21 <zivklein@Zivs-Air.lan>
2024-12-07 13:13:51 +0200

    start working on login activity

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        // Add other repositories if needed
+    }
+    dependencies {
+        // Other dependencies
+        classpath "androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7"
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
